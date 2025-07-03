53ead4101d28081db94ae4ddaa68ef0b3508a6e5
santosh12197 <santoshkumar12197@gmail.com>
2025-07-03 17:34:55 +0530

    model added for reset password

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.utils import timezone
 
 
 
@@ -102,3 +103,17 @@ class UserLocation(models.Model):
     class Meta:
         verbose_name = "User Location"
         verbose_name_plural = "User Location"
+
+
+class PasswordResetOTP(models.Model):
+    """
+        Model to reset password 
+    """
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reset_otps')
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
+    def is_expired(self):
+        # 10 minutes expiry
+        return timezone.now() > self.created_at + timezone.timedelta(minutes=10)
